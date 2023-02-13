905d99c5c308c606df8093d6f10a41f8e076c40a
TusharJain128 <tusharjain128@gmail.com>
2023-02-13 23:17:27 +0530

    Google

---
+++ src/components/WordBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function WordBox() {
+  return (
+    <>
+      <input
+        style={{
+          position: 'relative',
+          width: '300px',
+          fontSize: '20px',
+          borderRadius: '5px',
+        }}
+      />
+    </>
+  );
+}
+
+export default WordBox;
