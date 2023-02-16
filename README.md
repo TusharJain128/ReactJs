26ebc454e40a18c456c6840c54e61a16358af346
TusharJain128 <tusharjain128@gmail.com>
2023-02-16 19:55:12 +0530

    simple counter

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ReactJs
 
-## Assignment-2 
+## Assignment-4 
 
-Create a page with a textarea, where a user writes their introduction/bio in mixed case. There will be a button on click of which the bio will change to upper case and show on a paragraph element below the textarea.
+You will build a simple React App which increments/decrements count on-screen when buttons are clicked using React State.
--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, {useState} from "react";
+import React from "react";
 import "./style.css";
 
-export default function App() {
-    let [bio, setBio] = useState('')
-    return (
+export default class App extends React.Component{
+  constructor(){
+    super()
+      this.state={
+        count:0
+      }
+  }
+  increment(){
+    this.setState({count : this.state.count+1})
+  }
+  decrement(){
+    this.setState({count : this.state.count-1})
+  }
+  reset(){
+    this.setState({count: 0})
+  }
+  render(){
+    return(
       <div>
-
-        <textArea value={bio} onChange={(e)=>{setBio(e.target.value)}}>
-        </textArea><br/>
-
-        <button onClick={ ()=>{
-               let newBio= bio
-               setBio(newBio.toUpperCase())}}>Submit</button>
-
-        <p>{bio}</p>
-        
+        <button onClick={()=>this.increment()}>increase</button><br/>
+        <h1>{this.state.count}</h1><br/>
+        <button onClick={()=>this.decrement()}>decrease</button>
+        <button onClick={()=>this.reset()}>reset</button>
       </div>
     )
+  }
 }
