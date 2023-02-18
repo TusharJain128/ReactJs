def8836ccc2f0dc0f6737e277d028df4e3fcbef8
TusharJain128 <118379406+TusharJain128@users.noreply.github.com>
2023-02-18 14:49:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
-export default class App extends React.Component{
-  constructor(){
-    super()
-      this.state={
-        count:0
-      }
-  }
-  increment(){
-    this.setState({count : this.state.count+1})
-  }
-  decrement(){
-    this.setState({count : this.state.count-1})
-  }
-  reset(){
-    this.setState({count: 0})
-  }
-  render(){
-    return(
-      <div>
-        <button onClick={()=>this.increment()}>increase</button><br/>
-        <h1>{this.state.count}</h1><br/>
-        <button onClick={()=>this.decrement()}>decrease</button>
-        <button onClick={()=>this.reset()}>reset</button>
-      </div>
-    )
-  }
+export default function App() {
+  let [count, setCount]= useState(0)
+  setInterval(()=>{
+    return setCount(count = count+1)
+  },1000)
+  
+  return (
+    <div>
+       <p>{count}</p>
+    </div>
+  );
 }
