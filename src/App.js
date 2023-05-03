5c608a2d9b1194d9c665576f09b8f3ee0c480804
TusharJain128 <118379406+TusharJain128@users.noreply.github.com>
2023-05-03 15:17:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,88 +1,15 @@
-import './app.css';
-import React, { useState } from 'react';
+import React from "react";
+import axios from 'axios'
+import "./style.css";
 
-function App() {
-  const [fullName, setFullName] = useState('');
-  const [emailId, setEmailId] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [birthPlace, setBirthPlace] = useState('');
-  const [studentData, setStudentData] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const student = { fullName, emailId, phoneNumber, birthPlace };
-    setStudentData([...studentData, student]);
-  };
-
+export default function App() {
+  const data = axios.get("https://quizapi.io/api/v1/questions")
   return (
-    <div className="App">
+    <div>
+      <h1 align="center">Quiz</h1>
       <div>
-        <h1>Student Form</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            placeholder="Full Name"
-            type="text"
-            onChange={(e) => {
-              setFullName(e.target.value);
-            }}
-          />{' '}
-          <br /> <br />
-          <input
-            placeholder="Email Id"
-            type="email"
-            onChange={(e) => {
-              setEmailId(e.target.value);
-            }}
-          />{' '}
-          <br /> <br />
-          <input
-            placeholder="Phone Number"
-            type="tel"
-            onChange={(e) => {
-              setPhoneNumber(e.target.value);
-            }}
-          />{' '}
-          <br /> <br />
-          <input
-            placeholder="Birth Place"
-            type="text"
-            onChange={(e) => {
-              setBirthPlace(e.target.value);
-            }}
-          />{' '}
-          <br /> <br />
-        </form>
-        <button onClick={handleSubmit}>Submit</button>
-      </div>
-
-      <h1>Student's Data</h1>
-      <div className="down">
-        <table style={{ width: '70%' }}>
-          <thead>
-            <tr>
-              <th>Sr.No</th>
-              <th>Full Name</th>
-              <th>Email Id</th>
-              <th>Phone Number</th>
-              <th>Birth Place</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {studentData.map((data, i) => (
-              <tr key={i}>
-                <td>{i + 1}</td>
-                <td>{data.fullName}</td>
-                <td>{data.emailId}</td>
-                <td>{data.phoneNumber}</td>
-                <td>{data.birthPlace}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        {data}
       </div>
     </div>
   );
 }
-
-export default App;
